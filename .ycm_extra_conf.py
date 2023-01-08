29354d80f353b28c8f1c78ffd8562ffea5ad5bf2
Florian Fontan <dev@florian-fontan.fr>
2023-01-08 11:50:20 +0100

    Update WORKSPACE

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -3,20 +3,35 @@ def Settings(**kwargs):
             'flags': [
                 '-x', 'c++',
                 '-Wall', '-Wextra', '-Werror',
-                '-DCPLEX_FOUND',
-                '-DCOINOR_FOUND',
-                '-DIL_STD',  # Cplex
                 '-I', '.',
-                '-I', './bazel-coloringsolver/external/json/single_include',
-                '-I', './bazel-coloringsolver/external/googletest/googletest/include',
-                '-I', './bazel-coloringsolver/external/boost/',
-                '-I', './bazel-coloringsolver/external/optimizationtools',
-                # '-I', './../optimizationtools/',
-                '-I', './bazel-coloringsolver/external/columngenerationsolver',
-                # '-I', './../columngenerationsolver/',
-                '-I', './bazel-coloringsolver/external/stablesolver/',
-                # '-I', './../stablesolver/',
-                '-I', './bazel-coloringsolver/external/localsearchsolver/',
+
+                '-I', './bazel-coloringsolver/external/'
+                'json/single_include/',
+
+                '-I', './bazel-coloringsolver/external/'
+                'googletest/googletest/include/',
+
+                '-I', './bazel-coloringsolver/external/'
+                'boost/',
+
+                # optimizationtools
+                '-I', './bazel-coloringsolver/external/'
+                # '-I', './../'
+                'optimizationtools/',
+
+                # columngenerationsolver
+                '-I', './bazel-coloringsolver/external/'
+                # '-I', './../'
+                'columngenerationsolver/',
+
+                # stablesolver
+                '-I', './bazel-coloringsolver/external/'
+                # '-I', './../'
+                'stablesolver/',
+
+                # CPLEX
+                '-DCPLEX_FOUND',
+                '-DIL_STD',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/concert/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cplex/include/',
                 '-I', '/opt/ibm/ILOG/CPLEX_Studio129/cpoptimizer/include/',
